f11c51a39063d4dc71ab7b0196a2789220db9339
Duc <duc.t.cit21@eiu.edu.vn>
2024-11-19 12:58:24 +0700

    Update zod.ts

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -1,4 +1,4 @@
-import { object, string } from "zod"
+import { object, string, z } from "zod"
 
 export const signInSchema = object({
     email: string({ required_error: "Email is required" })
@@ -9,3 +9,31 @@ export const signInSchema = object({
         .min(8, "Password must be more than 8 characters")
         .max(32, "Password must be less than 32 characters"),
 })
+
+export const SignupFormSchema = z.object({
+    name: z
+        .string()
+        .min(2, { message: 'Name must be at least 2 characters long.' })
+        .trim(),
+    email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
+    password: z
+        .string()
+        .min(8, { message: 'Be at least 8 characters long' })
+        .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+        .regex(/[0-9]/, { message: 'Contain at least one number.' })
+        .regex(/[^a-zA-Z0-9]/, {
+            message: 'Contain at least one special character.',
+        })
+        .trim(),
+})
+
+export type FormState =
+    | {
+        errors?: {
+            //name?: string[]
+            email?: string[]
+            password?: string[]
+        }
+        message?: string
+    }
+    | undefined
